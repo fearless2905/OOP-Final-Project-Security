dade3c7a8556269c3f56f50c71eeb16d963c2822
restuwintama <alditorestu00@gmail.com>
2024-06-10 21:23:52 +0700

    coba

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ public class Main {
         SwingUtilities.invokeLater(() -> {
             JFrame frame = new JFrame("Jadwal Tugas Security UPN Veteran Jawa Timur");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setSize(800, 600);
+            frame.setSize(800, 800);
             frame.setLayout(new BorderLayout());
 
             JPanel topPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
