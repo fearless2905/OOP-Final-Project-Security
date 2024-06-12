54523259bb8881eeccbd7943407a8e595b9c382d
restuwintama <alditorestu00@gmail.com>
2024-06-12 13:41:28 +0700

    revisi

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableCellRenderer;
-import javax.swing.table.TableColumn;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -34,16 +32,12 @@ public class Main {
             tableModel = new DefaultTableModel(new Object[]{"Nama", "Lokasi", "Durasi (menit)", "Waktu Mulai", "Waktu Selesai", "Hapus"}, 0) {
                 @Override
                 public boolean isCellEditable(int row, int column) {
-                    return column == 5; // Hanya kolom "Hapus" yang bisa diedit (berisi tombol)
+                    return column == 5; // Tombol Hapus
                 }
             };
             scheduleTable.setModel(tableModel);
             
-            // Tambahkan renderer dan editor untuk tombol hapus
-            TableColumn column = scheduleTable.getColumnModel().getColumn(5);
-            column.setCellRenderer(new ButtonRenderer());
-            column.setCellEditor(new ButtonEditor(new JCheckBox(), scheduleTable));
-
+            // Menambahkan renderer dan editor untuk tombol hapus
             JScrollPane scrollPane = new JScrollPane(scheduleTable);
             centerPanel.add(scrollPane, BorderLayout.CENTER);
 
@@ -61,55 +55,3 @@ public class Main {
     }
 }
 
-// Renderer untuk tombol hapus
-class ButtonRenderer extends JButton implements TableCellRenderer {
-    public ButtonRenderer() {
-        setOpaque(true);
-    }
-
-    @Override
-    public Component getTableCellRendererComponent(JTable table, Object value,
-                                                   boolean isSelected, boolean hasFocus, int row, int column) {
-        setText((value == null) ? "Hapus" : value.toString());
-        return this;
-    }
-}
-
-// Editor untuk tombol hapus
-class ButtonEditor extends DefaultCellEditor {
-    protected JButton button;
-    private String label;
-    private JTable table;
-
-    public ButtonEditor(JCheckBox checkBox, JTable table) {
-        super(checkBox);
-        this.table = table;
-        button = new JButton();
-        button.setOpaque(true);
-        button.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                fireEditingStopped();
-                int selectedRow = table.convertRowIndexToModel(table.getEditingRow());
-                ((DefaultTableModel) table.getModel()).removeRow(selectedRow);
-            }
-        });
-    }
-
-    @Override
-    public Component getTableCellEditorComponent(JTable table, Object value,
-                                                 boolean isSelected, int row, int column) {
-        label = (value == null) ? "Hapus" : value.toString();
-        button.setText(label);
-        return button;
-    }
-
-    @Override
-    public Object getCellEditorValue() {
-        return new String(label);
-    }
-
-    @Override
-    protected void fireEditingStopped() {
-        super.fireEditingStopped();
-    }
-}
